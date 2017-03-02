cc0b3c266c51e5c01393a86f9f377f504f090e39
Chris Roberson <cdr67@yahoo.com>
2017-03-02 16:58:34 -0700

    Completed 1st Pass of apt-get jenkins

---
+++ apply.sh
@@ -0,0 +1,4 @@
+# Test Jenkins Changes
+export PATH=$PATH:/opt/puppetlabs/bin
+
+puppet apply puppet/manifests/site.pp --modulepath=/vagrant/puppet/modules/
--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -1,1 +1,35 @@
-include apt
+class jenkins {
+    # get key
+    exec { 'install_jenkins_key':
+        command => '/usr/bin/wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | /usr/bin/apt-key add -',
+    }
+
+    # source file
+    file { '/etc/apt/sources.list.d/jenkins.list':
+        content => "deb http://pkg.jenkins-ci.org/debian binary/\n",
+        mode    => '0644',
+        owner   => root,
+        group   => root,
+        require => Exec['install_jenkins_key'],
+    }
+
+    # update
+    exec { 'apt-get update':
+        command => '/usr/bin/apt-get update',
+        require => File['/etc/apt/sources.list.d/jenkins.list'],
+    }
+
+    # jenkins package
+    package { 'jenkins':
+        ensure  => latest,
+        require => Exec['apt-get update'],
+    }
+
+    # jenkins service
+    service { 'jenkins':
+        ensure  => running,
+        require => Package['jenkins'],
+    }
+}
+
+include jenkins
